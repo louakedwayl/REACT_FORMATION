503cc9594d2382551bd5aae54da11d923bc53321
Wayl Louaked <wlouaked@f6r13s1.clusters.42paris.fr>
2026-04-13 22:37:27 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-
-const title = "Bonjour les gens";
-const style = {color: "red", backgroundColor:"blue"}
-const todos = [
-  'Presenter react',
-  'Presenter le JSX',
-  'Creer des composants'
-]
+import { useState } from "react";
 
 function App() {
-  
-  const handleClick = () =>
-  {
-    alert();
-  }
 
-  return <>
-              <Title/>
-              <input type="text"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quis maiores ipsam incidunt, odio, facere perspiciatis iure repudiandae magni similique sapiente quisquam quaerat in cum reiciendis voluptate, veritatis earum expedita.</p>
-        <ul>
-          {todos.map(todo => (<li key={todo}>{todo}</li>))}
-        </ul>
-        </>;
+	const [firstname, setFirstName] = useState("John Doe")
+
+	const handleChange = (e) =>
+	{
+		setFirstName(e.target.value)
+	}
+
+	return  <form>
+				<input type="text" name= "firstname" value={firstname} onChange={handleChange}/>
+				{firstname}
+			</form>;
 }
 
 
-function Title()
-{
-  return <h1>Bonjour les Gens</h1>;
-}
 
 export default App
